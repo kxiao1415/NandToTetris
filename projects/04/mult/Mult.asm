bf5b06ccecd4a6f849099208fa01ddaaa255a5ff
kxiao1415 <k.xiao1415@gmail.com>
2015-10-25 18:55:34 -0400

    Project 04 Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,4 +6,34 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @R1
+    D=M
+    @y
+    M=D // y = R1
+
+    @R2
+    M=0 // initiate R2 to 0
+(LOOP)
+    @y
+    D=M
+    @END
+    D;JLE // if y <=0 goto END
+
+    @1
+    D=D-A
+    @y
+    M=D // y = y - 1
+
+    @R2
+    D=M
+    @R0
+    D=D+M
+    @R2
+    M=D // R2 = R2 + R0
+    
+    @LOOP
+    0;JMP // restart the LOOP
+
+(END)
+    @END
+    0;JMP // Infinite loop
